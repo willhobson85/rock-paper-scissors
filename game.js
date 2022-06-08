d0e21c69ba75940f05a508a818212b5a7ee48e3f
William Hobson <will.hobson85@gmail.com>
2022-06-08 16:23:38 -0400

    Build class framework

---
+++ game.js
@@ -0,0 +1,24 @@
+class Game {
+  constructor(gameType) {
+    this.gameType = gameType;
+    this.human = new Player('human');
+    this.computer = new Player('computer');
+  }
+
+  pickGame() {
+
+  }
+
+  checkWin() {
+
+  }
+
+  checkDraw() {
+
+  }
+
+  resetGame() {
+
+  }
+
+}
